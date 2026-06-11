3e95601658472fba97863cde7155959e53689862
senkuboy0-cyber <senkuboy0@gmail.com>
2026-06-11 14:50:11 +0600

    Fix build configuration - add kotlinx.serialization plugin and dependencies

--- ddgs/build.gradle.kts
+++ ddgs/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -58,6 +59,9 @@ dependencies {
     // Coroutines
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization.json)
 
     debugImplementation(libs.androidx.ui.tooling)
 }
